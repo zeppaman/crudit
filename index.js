5476eda20d3ea3bf3150145c4876c0058b9495c9
Daniele Fontani <d.fontani@sintraconsulting.eu>
2022-11-15 09:03:28 +0100

    refactor for npm packaging

---
+++ index.js
@@ -0,0 +1,8 @@
+import database from './src/database';
+import crudy from './src/crudy';
+import defaultConfig from './src/default';
+module.exports ={
+    database:database,
+    crudy:crudy,
+    defaultConfig:defaultConfig
+};
