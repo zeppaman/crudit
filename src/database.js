83de195ee160c9affe71010006156a21bc608b9c
Daniele Fontani <d.fontani@sintraconsulting.eu>
2022-11-16 19:08:35 +0100

    fixes

--- src/database.js
+++ src/database.js
@@ -39,7 +39,6 @@ let database= {
         return result;
     },
     search: async function (db, collection, query, projection={}, aggregate={}){
-        console.log("quering",query,projection,aggregate);
         if(aggregate   && Object.keys(aggregate).length === 0  && Object.getPrototypeOf(aggregate) === Object.prototype){
             return (await this.client.db(db).collection(collection).find(query).project(projection)).toArray();
         }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const crudy=require('./crudy');
 const database=require('./database');
 const defaultConfig=require('./default');
-console.log(crudy);
+
 exports.crudy=crudy;
 exports.database=database;
 exports.defaultConfig=defaultConfig;
